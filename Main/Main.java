fcc10ba0547a8c7f9325bb9215cd3eea88eff7b5
Malachi <91229989+MalachiHabib@users.noreply.github.com>
2022-05-18 19:32:41 +0930

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -5,6 +5,7 @@ import students.Farm;
 public class Main {
 
 	public static void main(String[] args) {
+		// input farm parameters, x coordinate, y coordinate, init money
 		Farm f = new Farm(10, 10, 10);
 		f.run();
 	}
